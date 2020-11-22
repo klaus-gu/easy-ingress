ac5418eba26ef49931eed144aa462d04ef4cc3e3
guyue375 <244017179@qq.com>
2020-11-22 13:48:58 +0800

    first commit

---
+++ plugin/zuul-plugin/src/main/java/xyz/turboklaus/zuul/plugin/ZuulPluginApplication.java
@@ -0,0 +1,13 @@
+package xyz.turboklaus.zuul.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulPluginApplication.class, args);
+    }
+
+}
