da32871f0c8282dcc99756e644286523be7d24d5
guyue375 <244017179@qq.com>
2020-11-23 22:06:13 +0800

    first commit

---
+++ integrations/src/main/java/xyz/turboklaus/integrations/zuul/ZuulResponsePostFilter.java
@@ -0,0 +1,32 @@
+package xyz.turboklaus.integrations.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+/**
+ * @author Turbo Klaus
+ * @author <a href="mailto:guyue375@outlook.com">klaus</a>
+ * @program easy-ingress
+ **/
+public class ZuulResponsePostFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
--- plugin/zuul-plugin/src/main/java/xyz/turboklaus/zuul/plugin/ZuulPluginApplication.java
+++ plugin/zuul-plugin/src/main/java/xyz/turboklaus/zuul/plugin/ZuulPluginApplication.java
@@ -3,6 +3,7 @@ package xyz.turboklaus.zuul.plugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ZuulPluginApplication {
 
